fa8067901410b09ea0342ce9f5cd4936d501a5f0
Nguyenhoangphuong <khangnguyen8196@gmail.com>
2022-11-02 11:46:34 +0700

    add_specialty_data

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -1,48 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-
+import { LANGUAGES } from '../../../utils';
 import Slider from "react-slick";
+import {getAllSpecialty} from '../../../services/userService'
 
 
 
 
 class Specialty extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSpecialty:[]
+        }
+    }
+
+    async componentDidMount (){
+       let res = await getAllSpecialty();
+       console.log (' check response:',res)
+        if (res && res.errCode === 0 ) {
+            this.setState({
+                dataSpecialty:res.data ? res.data : []
+            })
+        }
+    }
+
+    async componentDidUpdate(prevProps, prevState, snapshot)  {
+     if(this.props.language !== prevProps.language){
+
+        }
+    }
 
     render() {
+        let {dataSpecialty} = this.state;
+        let {language} = this.props
         return (
             <div className="section-share section-specialty">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
-                        <button className="btn-section">Xem thêm</button>
+                        <span className="title-section"><FormattedMessage id="homepage.specialty-popular"/></span>
+                        <button className="btn-section"><FormattedMessage id="homepage.more-infor"/></button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
-                                <div className="specialty-title">Cơ xương khớp 1</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
-                                <div className="specialty-title">Cơ xương khớp 2</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
-                                <div className="specialty-title">Cơ xương khớp 3</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
-                                <div className="specialty-title">Cơ xương khớp 4</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
-                                <div className="specialty-title">Cơ xương khớp 5</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
-                                <div className="specialty-title">Cơ xương khớp 6</div>
-                            </div>
+                        {dataSpecialty && dataSpecialty.length > 0 &&
+                            dataSpecialty.map((item, index)=>{
+                                return (
+                                    <div className="section-customize" key ={index} >
+                                        <div className="bg-image section-specialty"
+                                            style= {{backgroundImage: `url(${item.image})` }}
+                                        >       
+                                        </div>
+                                        <div className="specialty-title">{item.name}</div>
+                                    </div>
+                                 )
+                                }
+                            )
+                        } 
                         </Slider>
                     </div>
                     
--- src/services/userService.js
+++ src/services/userService.js
@@ -75,9 +75,13 @@ const createNewSpecialty = (data) => {
     return axios.post('/api/create-new-specialty', data);
 }
 
+const getAllSpecialty = () =>{
+    return axios.get(`/api/get-specialty`);
+}
+
 export  { handleLoginApi, getAllUsers, createNewUserService,
          deleteUserService, editUserService, getAllCodeService, getTopDoctorHomeService,
          getAllDoctors, saveDetailDoctorService, getDetailInforDoctorService, saveBulkScheduleDoctor,
          getScheduleDoctorByDate,getExtraInforDoctorById,getProfileDoctorById,postPatientBookAppointment,
-         postVerifyBookAppointment,createNewSpecialty,
+         postVerifyBookAppointment,createNewSpecialty,getAllSpecialty,
 };
